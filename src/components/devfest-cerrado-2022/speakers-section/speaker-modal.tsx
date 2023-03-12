52dfb0e22afc8727f83c41ce29ddd97987d70c5e
Tacio Medeiros <tacio.medeiros@vitta.me>
2023-03-12 17:20:50 -0300

    fix: fixes speaker card

--- src/components/devfest-cerrado-2022/speakers-section/speaker-modal.tsx
+++ src/components/devfest-cerrado-2022/speakers-section/speaker-modal.tsx
@@ -13,7 +13,7 @@ import {
 import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import styles from "../../styles/Speakers.module.css";
+import styles from "../../../styles/Speakers.module.css";
 import { Speaker } from "models/speaker";
 
 interface ModalProps {
