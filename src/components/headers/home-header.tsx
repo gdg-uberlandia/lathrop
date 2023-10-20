6c352bec475be49321eb4d8c5f45f16cd3dd0c0c
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-10-20 13:10:46 -0300

    feat: new header mobile

--- src/components/headers/home-header.tsx
+++ src/components/headers/home-header.tsx
@@ -1,48 +1,79 @@
 /*eslint-disable*/
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import styles from 'styles/Home.module.css';
-import configValues from "helpers/config";
-import { Col, Row } from "reactstrap";
-import Theme from "helpers/theme";
+import styles from './header.module.css'
+import LogoGDG from "assets/images/LogoGDG";
+import {
+    Collapse,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink,
+  } from 'reactstrap';
+import LogoMenu from "../../assets/images/MenuLogo"
+import { CloseMenu } from "../../assets/images/CloseMenu"
 
-interface HomeHeaderProps {
-}
-
-
-const HomeHeader: React.FC = ({ }) => {
-
-    const bgImageFullTheme = {
-        backgroundImage: `url(${Theme.bgImageFull})`,
-        backgrroundPosition: Theme.bgImageFullPosition,
-        backgroundSize: Theme.bgImageFullSize,
+const NAV_ITEMS = [
+    {
+        name: "O que é o Devfest?",
+        ref: "#about",
+    },
+    {
+        name: "Palestrantes",
+        ref: "#speakers",
+    },
+    {
+        name: "Patrocinadores",
+        ref: "#sponsors",
     }
+]
+
+const HomeHeader = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
 
     return (
-        <>
-            <div className={styles.BgWrap}>
-                <div className={styles.BgImageFull} />
-            </div>
-            <div className={styles.MainInnerFull}>
-                <div className={styles.MainInnerFullDescription}>
-                    <div style={{ paddingBottom: '30px', textAlign: 'center' }}>
-                        <h1>{configValues.placeCity}</h1>
-                        <h1>{configValues.formattedDate}</h1>
-                    </div>
-                    <div>
-                        <a className={styles.RegisterButton} href={configValues.eventLinkRegistrationUrl}>
-                            Inscreva-se
-                        </a>
-                    </div>
+        <header className={styles.Header}>
+            <LogoGDG height={20} width={224} />
 
-                </div>
-            </div >
-        </>
+            <nav className={styles.HeaderNavContainer}>
+                <ul className={styles.HeaderNav}>
+                    {NAV_ITEMS.map(({ ref, name }) => (
+                        <li key={name} className={styles.HeaderNavItem}>
+                            <a href={ref}>
+                                {name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+            <NavbarToggler className={styles.toggler_btn} onClick={toggle}>
+                <LogoMenu color="rgba(0,0,0,.55)" />
+            </NavbarToggler>
+
+            <Collapse isOpen navbar className={[styles.Collapse, isOpen && styles.HeaderNavContainerOpened].join(' ')}>
+                <NavbarToggler className={styles.toggler_btn} onClick={toggle}>
+                    <CloseMenu />
+                </NavbarToggler>
+
+                <Nav navbar>
+                    {NAV_ITEMS.map(({name, ref}, index) =>
+                        <NavItem key={"nav-item-" + index} className={styles.HeaderNavItem} onClick={toggle}>
+                            <NavLink href={ref}>
+                                {name}
+                            </NavLink>
+                        </NavItem>
+                    )}
+                </Nav>
+            </Collapse>
+
+            <a className={styles.HeaderSubscribeButton}>
+               Inscreva-se 
+            </a>
+        </header>
     );
 }
 
 export default HomeHeader;
-
-/*
-
-*/
