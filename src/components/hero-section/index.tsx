bb7f959ee12c51eb56ad7a2e75b9b8086545ba5f
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-10-21 23:50:20 -0300

    fix: hero in mobile

--- src/components/hero-section/index.tsx
+++ src/components/hero-section/index.tsx
@@ -11,29 +11,28 @@ export const HeroSection = () => {
   return (
     <section className={styles.Section}>
       <div className={styles.Content}>
-        <article className={styles.Texts}> 
-          <Image alt='Devfest Triângulo 2023' src={Title} loading="eager" layout="responsive" />
+        <Image alt='Devfest Triângulo 2023' src={Title} loading="eager" layout="responsive" />
 
-          <footer className={styles.IconSection}>
-            <span className={styles.IconItem}>
-              <Image alt='Um calendário na cor amarela feito em traços de giz' src={SchedulerIcon} width={44} height={32} />
-              <p>02 de Dezembro</p>
-            </span>
-
-            <span className={styles.IconItem}>
-              <Image alt='Um globo terrestre na cor amarela feito em traços de giz' src={GlobalIcon} width={44} height={32} />
-              <p>Uberlândia - MG</p>
-            </span>
-          </footer>
-        </article>
-
+          
         <aside className={styles.Aside}>
-          <p>Inscreva-se</p>
+          <p className={styles.AsideText}>Inscreva-se</p>
           <a href={configValues.eventLinkRegistrationUrl} target="_blank" className={styles.AsideButton}>
             Garantir meu ingresso
           </a>
           <Image className={styles.ArrowIcon} alt='Uma seta amarela feita em traços de giz apontando para o canto superior esquerdo' src={ArrowIcon} height={44} width={40} objectFit="cover" />
         </aside>
+
+        <footer className={styles.IconSection}>
+          <span className={styles.IconItem}>
+            <Image alt='Um calendário na cor amarela feito em traços de giz' src={SchedulerIcon} width={44} height={32} />
+            <p>02 de Dezembro</p>
+          </span>
+
+          <span className={styles.IconItem}>
+            <Image alt='Um globo terrestre na cor amarela feito em traços de giz' src={GlobalIcon} width={44} height={32} />
+            <p>Uberlândia - MG</p>
+          </span>
+        </footer>
       </div>
     </section>
   )
