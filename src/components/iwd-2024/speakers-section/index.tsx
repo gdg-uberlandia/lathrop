ca6ebacfb66f6a7a66237a7f0b2e2295ef4877e9
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-02-14 10:28:13 -0300

    speakers section desktop

--- src/components/iwd-2024/speakers-section/index.tsx
+++ src/components/iwd-2024/speakers-section/index.tsx
@@ -1,14 +1,31 @@
 import { Speaker } from "models/speaker";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SpeakerSection.module.css";
 import SpeakerCard from "./speaker-card";
-import { Col, Container, Row } from "reactstrap";
+import { Carousel, CarouselControl, CarouselItem, Col, Container, Row } from "reactstrap";
+import Image from "next/image";
+import clsx from "clsx";
 
 interface SpeakersSectionProps {
   speakers: Array<Speaker>,
 }
 
 const SpeakerSection = ({ speakers }: SpeakersSectionProps) => {
+  const [activeIndex, setActiveIndex] = useState(1);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === speakers.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? speakers.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+  
   return (
     <Container className={styles.Container}>
         <Row noGutters tag="header" className={styles.Header}>
@@ -21,13 +38,61 @@ const SpeakerSection = ({ speakers }: SpeakersSectionProps) => {
             No <strong>IWD Uberlândia 2024</strong>, teremos um time poderoso de palestrantes, composto por mulheres! Essas mulheres são destaque em diferentes áreas, e nosso objetivo vai além de estimular a comunidade tecnológica, abrangendo interação, conexão e Containerersidade.
           </Col>
         </Row>
-        <Row tag="section">
-          {speakers.map((speaker) => 
-            <SpeakerCard
-              key={speaker.slug}
-              speaker={speaker}
-            />
-          )}
+        <Row tag="section" className={styles.Content}>
+          <Carousel
+            interval={false}
+            activeIndex={activeIndex}
+            next={next}
+            previous={previous}
+            className={styles.SpeakersCarousel}
+            cssModule={{
+              'carousel-inner': styles.CarouselInner,
+            }}
+          >
+            {speakers.map((speaker, i) => 
+              <CarouselItem 
+                key={speaker.id} 
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                cssModule={{
+                  'carousel-item': styles.CarouselItem,
+                  'active': styles.CarouselItemActive,
+                }}
+              >
+                <SpeakerCard
+                  key={speaker.slug}
+                  speaker={speaker}
+                  color={
+                    i % 3 === 0 ? 'primary' : 
+                    i % 2 === 0 ? 'secondary' : 
+                    'tertiary'
+                  }
+                  active={i % 2 !== 0}
+                />
+              </CarouselItem>
+            )}
+            <button 
+              aria-label="Ver palestrantes anteriores"
+              onClick={previous}
+              className={clsx(
+                styles.SpeakersCarouselControl,
+                styles.SpeakersCarouselControlBack
+              )}
+            >
+              <Image alt="Ver palestrantes anteriores" src='/icons/arrow-back.svg' width={20} height={20} />
+            </button>
+            
+            <button 
+              aria-label="Ver próximos palestrantes"
+              onClick={next}
+              className={clsx(
+                styles.SpeakersCarouselControl,
+                styles.SpeakersCarouselControlForward
+              )}
+            >
+              <Image alt="Ver próximos palestrantes" src='/icons/arrow-forward.svg' width={20} height={20} />
+            </button>
+          </Carousel>
         </Row>
     </Container>
   );
