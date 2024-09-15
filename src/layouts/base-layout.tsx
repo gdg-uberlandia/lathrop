25fafed8b751798bfc80aa698506edbcb0f1660d
Lucas Vilaboim <lucas@vilaboim.com>
2024-09-15 13:43:49 -0300

    feat: add open sans font from next

--- src/layouts/base-layout.tsx
+++ src/layouts/base-layout.tsx
@@ -1,36 +1,37 @@
 import React, { ReactNode } from "react";
-import Head from 'next/head'
-import Footer from '../components/footers/footer'
-import NavbarHome from '../components/navbar/home-navbar'
+import Head from "next/head";
+import Footer from "../components/footers/footer";
 import configValues from "helpers/config";
+import { Open_Sans } from "next/font/google";
+
+const open = Open_Sans({ subsets: ["latin"], variable: "--font-open-sans" });
 
 interface BaseLayout {
-    children: ReactNode;
+  children: ReactNode;
 }
+
 //id="base-layout" className="main-content"
 const BaseLayout: React.FC<BaseLayout> = ({ children }) => {
+  const favicon = `${configValues.organizedBy}/favicon.ico`;
 
-    const favicon = `${configValues.organizedBy}/favicon.ico`
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="functions-insert-dynamic-og"></meta>
+        <link rel="icon" href={favicon} sizes="any" />
+        <title>{configValues.name}</title>
+      </Head>
 
-    return (
-        <>
-            <Head>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
-                />
-                <meta name="functions-insert-dynamic-og"></meta>
-                <link rel="icon" href={favicon} sizes="any" />
-                <title>{configValues.name}</title>
-            </Head>
-            <main className="absolute-position">
-                {children}
-                <Footer />
-            </main>
-        </>
-    );
-}
-
-
+      <main className={`${open.variable} font-sans absolute-position`}>
+        {children}
+        <Footer />
+      </main>
+    </>
+  );
+};
 
 export default BaseLayout;
