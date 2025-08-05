9141c6cd024c727fe6ccf2e25a66beacffdd1788
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-08-05 06:52:31 -0300

    use google sans font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,21 +10,6 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#000000" />
           <link rel="shortcut icon" />
           <link rel="apple-touch-icon" sizes="76x76" />
-          {/* Fonts and icons */}
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Mono&display=swap"
-            rel="stylesheet"
-          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
