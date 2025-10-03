ee5c28d3c4a38ac7685b48eb3dfb8792b660c3fc
Tacio Medeiros <taciomedeiros@gmail.com>
2025-10-03 11:53:12 -0300

    fix: correctly format Firebase private key by replacing escaped newlines

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -30,7 +30,10 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(
+  /\\n/g,
+ '\n',
+),
     }),
     databaseURL: process.env.FIREBASE_ADMIN_DATABASE_URL,
   };
