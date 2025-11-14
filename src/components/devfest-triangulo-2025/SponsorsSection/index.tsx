590c32fb94206609e5ff75f55b8298bea1afdb20
Heldson Luiz da Silva <heldsonluiz@gmail.com>
2025-11-14 11:52:59 -0300

    fix: fix sponsors sizes

--- src/components/devfest-triangulo-2025/SponsorsSection/index.tsx
+++ src/components/devfest-triangulo-2025/SponsorsSection/index.tsx
@@ -150,11 +150,11 @@ const SponsorLevel = ({ sponsorLevel: { name, items } }: SponsorLevelProps) => {
       {items.length > 0 && (
         <section className="flex flex-col items-center w-full">
           <Tag>{name}</Tag>
-          <div className="flex flex-row flex-wrap gap-x-16 gap-y-5 items-center justify-center w-full">
+          <div className="flex flex-row flex-wrap gap-x-16 gap-y-5 items-center justify-center w-full mt-4">
             {items.map((item) => (
               <a href={item.url} target="_blank" key={item.name}>
                 <div
-                  className={`w-48 h-24 relative max-h-[80px] max-w-full ${styles[item.level]}`}
+                  className={`relative ${styles[item.level]} ${styles[item.format]}`}
                 >
                   <Image
                     className={clsx(styles.SponsorImage)}
