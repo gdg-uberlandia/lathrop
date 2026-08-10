9fff40b9bd9fcb4d1a58d2aac273afa88f3a2c9e
Heldson Luiz da Silva <heldsonluiz@gmail.com>
2026-08-10 17:46:39 -0300

    perf: isolate authentication from public routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,28 +3,39 @@ import type { AppContext, AppInitialProps } from "next/app";
 
 import React, { ReactNode } from "react";
 import { NextComponentType } from "next";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
 
 import GoogleAnalytics from "../components/google-analytics";
 import { AppLayoutProps } from "../../types";
-import { AuthProvider } from "context/AuthContext";
+
+const AuthProvider = dynamic(() =>
+  import("context/AuthContext").then((module) => module.AuthProvider),
+);
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = (
   props: AppLayoutProps,
 ) => {
   const { Component, pageProps } = props;
+  const router = useRouter();
 
   const Layout =
     Component.layout ||
     (({ children }: { children: ReactNode }) => <>{children}</>);
 
-  return (
-    <AuthProvider>
+  const page = (
+    <>
       <GoogleAnalytics />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </AuthProvider>
+    </>
   );
+
+  const requiresAuth =
+    router.pathname === "/login" || router.pathname.startsWith("/admin");
+
+  return requiresAuth ? <AuthProvider>{page}</AuthProvider> : page;
 };
 
 export default MyApp;
